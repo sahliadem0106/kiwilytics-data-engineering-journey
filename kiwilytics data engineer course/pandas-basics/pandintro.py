68b98f0b8cdd9c11004cf8c81f9463c252821e80
sahliadem0106 <sahliadem0106@gmail.com>
2025-09-07 21:29:55 +0100

    Delete kiwilytics data engineer course directory

--- kiwilytics data engineer course/pandas-basics/pandintro.py
+++
@@ -1,5 +0,0 @@
-#PANDAS
-#diff between cleaning and transformation
-#cleaning : handling missing values , duplicates, incorrect types , malformed data , replacement ,deleting ... etc
-#transformation : changing structure , aggregating , formatting , and engineering new fields
-# * Pandas does both , and we'ill practice it step by step
